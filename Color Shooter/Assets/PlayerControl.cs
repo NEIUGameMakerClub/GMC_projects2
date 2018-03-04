5539ee1a18920b50434a0994abd2a2f5f2d87d4d
NevrinO <nevrin1011@gmail.com>
2018-03-03 19:58:11 -0600

    V0.1 of project
    This is the assets I created for this project. also Bundled up into an asset pack for easy loading into editor

---
+++ Color Shooter/Assets/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerControl: MonoBehaviour
+{
+	public GameObject mainCamera;
+	public float rotationSpeed;
+	public float movementSpeed;
+
+	public float maxUpRotation = -70;
+	public float maxDownRotation = 70;
+	private float rotX = 0.0f;
+
+	void Start(){
+		Vector3 rot = mainCamera.transform.localRotation.eulerAngles;
+		rotX = rot.x;
+	}
+	void FixedUpdate()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+		float h = rotationSpeed * Input.GetAxis("Mouse X");
+		float v = rotationSpeed * -Input.GetAxis("Mouse Y");
+
+		rotX += v;
+
+		rotX = Mathf.Clamp(rotX, maxUpRotation, maxDownRotation);
+
+		Quaternion newRotation = Quaternion.Euler(rotX, 0.0f, 0.0f);
+		mainCamera.transform.localRotation = newRotation;
+		transform.Rotate(0, h, 0);
+		transform.Translate(x, 0, z);
+
+
+	}
+}
---
+++ Color Shooter/Assets/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public GameObject enemy;
+	public float rate;
+	public int max;
+	public int current;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		current = 0;
+		timer = rate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer = timer - Time.deltaTime;
+		if(timer <= 0 && current < max){
+			Instantiate (enemy, new Vector3(Random.Range (-80, 81), 10, Random.Range (-80, 81)), new Quaternion(0,0,0,0));
+			timer = rate;
+			current++;
+		}
+	}
+}
