879f7d16914218a9348df6a52e26f655ecf52893
deepdishattack <36520677+deepdishattack@users.noreply.github.com>
2018-03-08 16:35:02 -0600

    Add files via upload

---
+++ EggMachine/Assets/follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+
+
+    public GameObject target = null;
+    private float homingSpeed = 5f;
+
+    // Use this for initialization
+    void Start () {
+        //target = GameObject.FindWithTag("Player");
+	}
+	
+
+        void Update() { 
+
+            
+                transform.LookAt(target.transform);
+                Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+                rb.velocity = transform.forward * homingSpeed;
+            
+        }
+
+}
