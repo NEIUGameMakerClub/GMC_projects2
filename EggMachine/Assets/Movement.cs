80fd536e0717b107d21eb53a455337ba28d8ef1b
deepdishattack <36520677+deepdishattack@users.noreply.github.com>
2018-03-08 16:06:53 -0600

    Update Movement.cs

--- EggMachine/Assets/Movement.cs
+++ EggMachine/Assets/Movement.cs
@@ -1,70 +1,79 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class controls : MonoBehaviour
 {
 
-	public float playerSpeed = 3;
 
-	public float yMovement;
-	public float xMovement;
-	public float zMovement;
+    public float xSensitivity = 100.0f;
+    private float rotationx = 0.0f;
 
+    public float playerSpeed = 3;
 
+    public float yMovement;
+    public float xMovement;
+    public float zMovement;
 
-	// Use this for initialization
-	void Start()
-	{
+    //private Rigidbody rb;
 
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 rotation = transform.localRotation.eulerAngles;
+        rotationx = rotation.x;
+        //rb = GetComponent<Rigidbody>();
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
+    // Update is called once per frame
+    void Update()
+    {
 
+        float mouseX = Input.GetAxis("Mouse X");
+        rotationx += mouseX * xSensitivity * Time.deltaTime;
 
-		//bool spaceHeld = Input.GetButton("Space");
-		bool rightHeld = Input.GetButton("right");
-		bool leftHeld = Input.GetButton("left");
-		bool upHeld = Input.GetButton("up");
-		bool downHeld = Input.GetButton("down");
-
-
-
-
-		if (rightHeld)
-		{
-			if (leftHeld)
-				xMovement = 0.0f;
-			else
-				xMovement = 1.0f;
-		}
-		else
-		{
-			if (leftHeld)
-				xMovement = -1.0f;
-			else
-				xMovement = 0.0f;
-		}
-
-		if (downHeld)
-		{
-			if (upHeld)
-				zMovement = 0.0f;
-			else
-				zMovement = -1.0f;
-		}
-		else
-		{
-			if (upHeld)
-				zMovement = 1.0f;
-			else
-				zMovement = 0.0f;
-		}
+        //bool spaceHeld = Input.GetButton("Space");
+        bool rightHeld = Input.GetButton("right");
+        bool leftHeld = Input.GetButton("left");
+        bool upHeld = Input.GetButton("up");
+        bool downHeld = Input.GetButton("down");
 
 
-		transform.Translate(xMovement * Time.deltaTime * playerSpeed, yMovement, zMovement * Time.deltaTime * playerSpeed);
+        
 
-	}
+        if (rightHeld)
+        {
+            if (leftHeld)
+                xMovement = 0.0f;
+            else
+                xMovement = 1.0f;
+        }
+        else
+        {
+            if (leftHeld)
+                xMovement = -1.0f;
+            else
+                xMovement = 0.0f;
+        }
+
+        if (downHeld)
+        {
+            if (upHeld)
+                zMovement = 0.0f;
+            else
+                zMovement = -1.0f;
+        }
+        else
+        {
+            if (upHeld)
+                zMovement = 1.0f;
+            else
+                zMovement = 0.0f;
+        }
+
+        Quaternion localRotation = Quaternion.Euler(0.0f, rotationX, 0.0f);
+        transform.rotation = localRotation;
+
+        transform.Translate(xMovement * Time.deltaTime * playerSpeed, yMovement, zMovement * Time.deltaTime * playerSpeed);
+    }
 }
