4aa4ad150dec62100e0b127848d96b201920597e
Claudiu Moise <claudiu.moise@gmail.com>
2018-03-08 16:22:16 -0600

    We'll bang, ok?

---
+++ testing/Assets/movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	//2
+	public float MoveSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//2
+		Move ();
+	}
+
+	/*Code for lesson 2
+	void Move (){
+		float movementZ = Input.GetAxis ("Vertical") * MoveSpeed * Time.deltaTime;
+		float movementX = Input.GetAxis ("Horizontal") * MoveSpeed * Time.deltaTime;
+
+		//reffers to the thing we attach to
+		transform.position += transform.forward * movementZ;
+		transform.position += transform.right * movementX;
+
+	
+
+
+	}
+	*/
+
+	//Code for lesson 3
+	float movementZ = Input.GetAxis ("Vertical") * MoveSpeed * Time.deltaTime;
+	float movementX = Input.GetAxis ("Horizontal") * MoveSpeed * Time.deltaTime;
+
+	Vector3 direction = new Vector3
+}
