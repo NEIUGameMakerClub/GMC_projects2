88c1dab8f9e5f5cd005eaa6e5da47df7baebf48f
cnmoise <claudiu.moise@gmail.com>
2018-03-31 23:03:11 -0500

    Edits to Color Shooter
    -made projectiles transparent

---
+++ Color Shooter/Assets/Scripts/Grabber.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour {
+
+	public float range = 40f;
+	public float force = 2000f;
+
+
+	private RaycastHit hit;
+	private GameObject pickedUpObject = null;
+
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		if(Input.GetButton ("Fire1")){
+
+			if(Physics.Raycast(transform.position, transform.forward, out hit , range)){
+
+				if(hit.collider.gameObject.tag=="Enemy"){ //add collider reference otherwise you can't access gameObject!
+
+					pickedUpObject = hit.collider.gameObject; 
+					hit.collider.gameObject.transform.parent= transform;
+
+					//Makes the grabbed object transparent
+					//had to change shader to legacy Transparent/Diffuse
+					var grabcolor = hit.collider.gameObject.GetComponent<Renderer>().material.color;
+					var newColor = new Color(grabcolor.r, grabcolor.g, grabcolor.b, 0.5f);
+					hit.collider.gameObject.GetComponent<Renderer>().material.color = newColor;
+
+					hit.collider.gameObject.transform.position=transform.position + transform.forward + transform.forward + transform.up; 
+					//pickedUpObject.GetComponent<Rigidbody>().isKinematic = true;
+				}
+			}
+		} else { //i think a regular else statement is fine
+			if (pickedUpObject != null) {
+				//horrible hack i have no idea how assignment works
+				var origColor = pickedUpObject.GetComponent<Renderer> ().material.color;
+				pickedUpObject.GetComponent<Renderer> ().material.color = new Color(origColor.r, origColor.g, origColor.b, 1);
+
+				pickedUpObject.transform.parent = null; 
+				pickedUpObject.GetComponent<Rigidbody> ().AddForce(transform.forward * force, ForceMode.VelocityChange);
+				//pickedUpObject.GetComponent<Rigidbody>().isKinematic = false;
+				pickedUpObject = null;
+			}
+		}
+	}
+}
