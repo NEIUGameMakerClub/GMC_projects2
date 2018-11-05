c5f60af60f6ad536a8133f16c6d945354eea5d28
cnmoise <claudiu.moise@gmail.com>
2018-11-05 09:41:21 -0600

    Tutorial

---
+++ Survival/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+    private int level = 3;
+    public static GameManager instance = null;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+
+    void Awake() {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    public void GameOver()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
